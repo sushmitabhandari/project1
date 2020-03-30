e7afa45ff8c740e04c42033c771d25c41f719c39
sushmitabhandari <42686549+sushmitabhandari@users.noreply.github.com>
2020-03-30 15:17:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "master" {
   ami           = "ami-0620d12a9cf777c87"
   instance_type = "t2.micro"
   key_name                    = "home"
-  security_groups      = "sg-0659fb504c0a46352"
+  security_groups      = ["sg-0659fb504c0a46352"]
   subnet_id                   = "var.subnet1"
   associate_public_ip_address = "true"
   
