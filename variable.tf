70a8d4e3d8dae69d5764e03fd07c1c5e853d1b8c
sushmitabhandari <42686549+sushmitabhandari@users.noreply.github.com>
2020-03-30 12:08:30 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,13 @@
+variable "ami_id" {
+  description = "AMI ID"
+  default = "ami-0620d12a9cf777c87"
+}
+
+variable "subnet1" {
+  description = "Subnet 1"
+  default = "subnet-001d87dc84073b441"
+}
+variable "subnet2" {
+  description = "Subnet 2"
+  default = "subnet-02d7597a7038de3b8"
+}
