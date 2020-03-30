01b863cdb49e6ffd6a0f5a25a3daaa5c997614d5
sushmitabhandari <42686549+sushmitabhandari@users.noreply.github.com>
2020-03-30 12:18:23 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,5 +1,5 @@
 variable "ami_id" {
-  description = "AMI ID"
+  description = "AMI ID"sg-0659fb504c0a46352
   default = "ami-0620d12a9cf777c87"
 }
 
@@ -11,3 +11,8 @@ variable "subnet2" {
   description = "Subnet 2"
   default = "subnet-02d7597a7038de3b8"
 }
+variable "security_group" {
+  description = "Security group"
+  default = "sg-0659fb504c0a46352"
+}
+
