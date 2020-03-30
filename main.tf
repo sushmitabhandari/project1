acebf7baa1086dc1411bf644428ded4c4b085388
sushmitabhandari <42686549+sushmitabhandari@users.noreply.github.com>
2020-03-30 17:37:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,30 @@
 resource "aws_instance" "master" {
   ami           = "ami-0620d12a9cf777c87"
+  instance_type = "t2.medium"
+  key_name                    = "home"
+  security_groups      = ["sg-0659fb504c0a46352"]
+  subnet_id                   = "subnet-001d87dc84073b441"
+  associate_public_ip_address = "true"
+  user_data = <<-EOF
+		#!/bin/bash
+                sudo su
+		sudo apt-get update
+		sudo touch /home/ubuntu/make.c
+		sudo apt-get install docker.io -y
+		sudo systemctl start docker.io
+                sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+                sudo apt install curl
+                sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+                sudo apt install kubeadm -y
+                sudo swapoff -a
+                sudo hostnamectl set-hostname master-node       
+	EOF
+  tags = { 
+    Name = "master1"
+  }
+}
+resource "aws_instance" "slave" {
+  ami           = "ami-0620d12a9cf777c87"
   instance_type = "t2.micro"
   key_name                    = "home"
   security_groups      = ["sg-0659fb504c0a46352"]
@@ -7,13 +32,19 @@ resource "aws_instance" "master" {
   associate_public_ip_address = "true"
   user_data = <<-EOF
 		#!/bin/bash
-        sudo su
+                sudo su
 		sudo apt-get update
 		sudo touch /home/ubuntu/make.c
 		sudo apt-get install docker.io -y
 		sudo systemctl start docker.io
+                sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+                sudo apt install curl
+                sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+                sudo apt install kubeadm -y
+                sudo swapoff -a
+                sudo hostnamectl set-hostname slave-node       
 	EOF
   tags = { 
-    Name = "master1"
+    Name = "slave1"
   }
 }
