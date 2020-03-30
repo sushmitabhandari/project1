59ef0b0939bb894ac4357b1bcd250c1f6122d48e
sushmitabhandari <42686549+sushmitabhandari@users.noreply.github.com>
2020-03-30 17:00:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,14 @@ resource "aws_instance" "master" {
   security_groups      = ["sg-0659fb504c0a46352"]
   subnet_id                   = "subnet-001d87dc84073b441"
   associate_public_ip_address = "true"
-  user_data = "3bb87e481fbaf94b8e"
+  user_data = <<-EOF
+		#!/bin/bash
+        sudo su
+		sudo apt-get update
+		sudo touch /home/ubuntu/make.c
+		sudo apt-get install -y docker
+		sudo systemctl start docker
+	EOF
   tags = { 
     Name = "master1"
   }
