4c19a1a28796a71ff83a31575ad052b81ae0830e
sushmitabhandari <42686549+sushmitabhandari@users.noreply.github.com>
2020-03-30 13:30:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "master" {
   ami           = "ami-0620d12a9cf777c87"
   instance_type = "t2.micro"
   key_name                    = "home"
-  vpc_security_group_ids      = var.security_group
+  security_groups      = var.security_group
   subnet_id                   = var.subnet1
   associate_public_ip_address = "true"
   
